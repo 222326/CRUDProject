aa222dff5585a26683befd70b5e47bcf4de00432
222326 <35493822+222326@users.noreply.github.com>
2018-05-29 09:01:40 -0500

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "zachsusee14";
+$password = "zach0902";
+$dbname = "music";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to delete a record
+$sql = "DELETE FROM Music WHERE id=3";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
